12186f0ffcfc2f67e2d6ae5e25c174310c946dc1
Ront31 <kolya.maslov.09@gmail.com>
2023-02-28 16:27:29 +0200

    some update in task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const divGalleryEl = document.querySelector(".gallery");
+const galleryMurkup = createGallery(galleryItems);
+divGalleryEl.insertAdjacentHTML("beforeend", galleryMurkup);
+
+divGalleryEl.addEventListener("click", onGalleryImgClick);
+
+function createGallery(arg) {
+  return arg
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="Image description"
+            />
+          </a>
+        </div>
+        `;
+    })
+    .join("");
+}
+
+function onGalleryImgClick(e) {
+  e.preventDefault();
+  console.log(e.target.getAttribute("data-source"));
+}
